992d613946e8f2a012e4249cda7556825d779c24
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-06-15 19:17:48 +0530

    Color Related Basic Constants File Added With Data

---
+++ lib/constants/colours.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color colorRed = Colors.red;
+const Color colorBlue = Colors.blue;
+const Color colorBlack = Colors.black;
+const Color colorGrey = Colors.grey;
+const Color colorBg = Colors.white;
