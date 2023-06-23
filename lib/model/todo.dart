88fe35b238d502dcf2e016d1fb9f10f8b6f4c838
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-06-23 21:52:15 +0530

    Data Updated - Task (Default)

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -11,14 +11,14 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: 1, todoText: "Check Mail 1", isDone: true),
-      ToDo(id: 2, todoText: "Check Mail 2", isDone: false),
-      ToDo(id: 3, todoText: "Check Mail 3", isDone: true),
-      ToDo(id: 4, todoText: "Check Mail 4", isDone: false),
-      ToDo(id: 5, todoText: "Check Mail 5", isDone: true),
-      ToDo(id: 6, todoText: "Check Mail 6", isDone: false),
-      ToDo(id: 7, todoText: "Check Mail 7", isDone: true),
-      ToDo(id: 8, todoText: "Check Mail 8", isDone: false),
+      ToDo(id: 1, todoText: "Check Mail 01", isDone: true),
+      ToDo(id: 2, todoText: "Check Mail 02", isDone: false),
+      ToDo(id: 3, todoText: "Check Mail 03", isDone: true),
+      ToDo(id: 4, todoText: "Check Mail 04", isDone: false),
+      ToDo(id: 5, todoText: "Check Mail 05", isDone: true),
+      ToDo(id: 6, todoText: "Check Mail 06", isDone: false),
+      ToDo(id: 7, todoText: "Check Mail 07", isDone: true),
+      ToDo(id: 8, todoText: "Check Mail 08", isDone: false),
     ];
   }
 }
